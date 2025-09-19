2b961c4297d5fbf4076edadfa80daa3d8dafab8c
shiva <shiva>
2025-09-19 09:46:25 +0000

    Modified by Shiva

--- core/src/test/java/training/taylor/timetracker/core/TrackerTest.java
+++ core/src/test/java/training/taylor/timetracker/core/TrackerTest.java
@@ -3,28 +3,19 @@ package training.taylor.timetracker.core;
 import static org.junit.Assert.*;
 
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import training.taylor.timetracker.core.dao.TimeEntry;
 
-/**
- * Created by Jason on 6/19/2015.
- */
-@RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(classes = TrackerCoreConfig.class)
 public class TrackerTest {
-    @Autowired
-    private Tracker tracker;
 
     @Test
     public void testMe() {
+        Tracker tracker = new Tracker();
         assertNotNull(tracker);
     }
 
     @Test
     public void testAdd() {
+        Tracker tracker = new Tracker();
         TimeEntry entry = new TimeEntry();
         entry.setDescription("Entry Test");
         entry.setRate(80.0f);
@@ -33,3 +24,4 @@ public class TrackerTest {
         assertTrue(tracker.size() > 0);
     }
 }
+
